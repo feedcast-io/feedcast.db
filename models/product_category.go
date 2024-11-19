23efa32ab00007a1c211072dd26eba34acc7791e
Romain <romain@orixamedia.com>
2024-11-19 16:22:22 +0100

    feat: add function to get product category hierarchy

--- models/product_category.go
+++ models/product_category.go
@@ -2,6 +2,7 @@ package models
 
 import (
 	"database/sql"
+	"gorm.io/gorm"
 	"strings"
 )
 
@@ -24,4 +25,38 @@ func (c *ProductCategory) GetTitleFromLang(lang string) string {
 	}
 
 	return title
+}
+
+func GetHierarchyCategory(conn *gorm.DB, googleCategoryId int32) []ProductCategory {
+	var categories []ProductCategory
+
+	var ids []int32
+
+	sql := `
+WITH RECURSIVE an AS (
+	SELECT pc.id, google_id, pc.parent_id
+	FROM product_category pc
+	WHERE pc.google_id = ?
+	UNION
+	SELECT pc.id, pc.google_id, pc.parent_id
+	FROM product_category pc
+	JOIN an ON an.parent_id = pc.id
+)
+SELECT google_id FROM an;
+`
+	conn.Raw(sql, googleCategoryId).Find(&ids)
+
+	for _, id := range ids {
+		var cat ProductCategory
+		if e := conn.Find(&cat, id).Error; nil == e {
+			categories = append(categories, cat)
+		}
+	}
+
+	// reverse array
+	for i, j := 0, len(categories)-1; i < j; i, j = i+1, j-1 {
+		categories[i], categories[j] = categories[j], categories[i]
+	}
+
+	return categories
 }
